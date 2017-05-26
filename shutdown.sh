681d903d7ca295e8c3fd55391a264e2883355680
Matt <ybotspawn@gmail.com>
2017-05-26 12:27:14 -0400

    Delete shutdown.sh

--- shutdown.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-ALIEN_PATH=/opt/AlienPyFX
-SCRIPT_PATH=/opt/AlienwareBattery
-
-source $SCRIPT_PATH/current_profile
-python2.7 $ALIEN_PATH/AlienPyFX_c.py --theme $ALIEN_PATH/afx-allgreen
